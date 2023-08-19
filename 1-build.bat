4b2fa359e84988931f92e835427fd9e136819ce3
ArcaneAria <arcanearia@gmail.com>
2023-08-19 16:40:08 -0700

    Added back in ANIM rename to make 3DO-ANIM happy for now.

--- 1-build.bat
+++ 1-build.bat
@@ -15,9 +15,9 @@ python tools\DialogueBoxesToCELs.py "generated_images\msg0-1-01" "generated_cels
 echo:
 
 echo Converting dialogue box CELs to ANIM...
-::rename "cd\working\BELZERION_ART\MSG0-1-01.C6" MSG0-1-01.C6.ANIM
+rename "cd\working\BELZERION_ART\MSG0-1-01.C6" MSG0-1-01.C6.ANIM
 tools\3DO-ANIM.exe -origanim "cd\working\BELZERION_ART\MSG0-1-01.C6.ANIM" -inputceldir "generated_cels\msg0-1-01" -includeplut FALSE -overwrite TRUE
-::rename "cd\working\BELZERION_ART\MSG0-1-01.C6.ANIM" MSG0-1-01.C6
+rename "cd\working\BELZERION_ART\MSG0-1-01.C6.ANIM" MSG0-1-01.C6
 echo:
 
 pause
